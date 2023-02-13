da19aeb59a575d3b93631444bd8b2c32dfcbfc64
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-02-13 20:13:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Cards from "./components/Cards/Cards";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1 className="text-2xl font-bold text-center p-4">Hi 👋</h1>
+      <Cards></Cards>
+      <a href="https://github.com/arghya20">
+        <div className="w-1/6 mx-auto justify-center pt-9 pb-28 cursor-pointer">
+          <span className="bg-yellow-400 hover:bg-yellow-500   flex justify-evenly text-2xl  p-2 rounded-lg font-bold outline transition-all">
+            Arghya Ghosh
+            <img
+              className="w-[30px]"
+              src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
+              alt=""
+            />
+          </span>
+        </div>
+      </a>
     </div>
   );
 }
